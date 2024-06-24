a37547ac096d3dd10f1b7eb05594558e758d3028
HueJo <slack_water@naver.com>
2024-06-24 17:28:56 +0900

    read

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,13 +6,17 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
 public class ArticleController {
-    @Autowired
+    @Autowired  // 리파지토리 구현객체 주입
     private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
@@ -36,5 +40,39 @@ public class ArticleController {
         log.info(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+        /**
+         * 1. id를 조회해 데이터 가져오기
+         * 2. 모델에 데이터 등록하기
+         * 3. 뷰 페이지 반환하기
+         */
+
+        // id를 조회해 데이터 가져오기
+        Article articleEntity =
+                articleRepository.findById(id).orElse((null));
+
+        // 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);  // article 이라는 이름으로 데이터 등록
+
+        // 뷰 페이지 반환하기
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        /**
+         * 1. 모든 데이터 가져오기
+         * 2. 모델에 데이터 등록하기
+         * 3. 뷰 페이지 설정하기
+         */
+
+        // 모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        model.addAttribute("articleList", articleEntityList);
+        return "articles/index";
     }
 }
